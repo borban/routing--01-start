49a7c7cad9e9e1591e2fb1003609373194b88404
Ben Orban <benjamin.orban@gmail.com>
2019-09-29 10:08:08 -0500

    Lesson 222

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Blog.css";
 import { Route } from "react-router-dom";
+import Posts from "./Posts/Posts";
 
 class Blog extends Component {
   render() {
@@ -18,8 +19,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home 2</h1>} />
+        {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
+        {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
