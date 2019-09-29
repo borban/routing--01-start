a8f3da48e9fb1adcaa9a4093b7e966b7e7ea1b52
Ben Orban <benjamin.orban@gmail.com>
2019-09-29 16:28:15 -0500

    Lesson 233

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./Blog.css";
-import { Route, NavLink } from "react-router-dom";
+import { Route, NavLink, Switch } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 import FullPost from "./FullPost/FullPost";
@@ -41,9 +41,11 @@ class Blog extends Component {
         </header>
         {/*<Route path="/" exact render={() => <h1>Home</h1>} />*/}
         {/*<Route path="/" render={() => <h1>Home 2</h1>} />*/}
-        <Route path="/" exact component={Posts} />
-        <Route path="/new-post" exact component={NewPost} />
-        <Route path="/:id" exact component={FullPost} />
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" exact component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
