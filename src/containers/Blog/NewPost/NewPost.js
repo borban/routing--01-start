215e8837722bdd033e3d118d5a87c9c503fed454
Ben Orban <benjamin.orban@ehi.com>
2019-10-08 08:15:45 -0500

    Lesson 239

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import {Redirect} from "react-router";
 
 import "./NewPost.css";
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
   state = {
     title: "",
     content: "",
-    author: "Max"
+    author: "Max",
+    submitted: false
   };
 
   postDataHandler = () => {
@@ -18,7 +20,9 @@ class NewPost extends Component {
     };
     axios.post("/posts", data).then(response => {
       console.log(response);
+      this.setState({submitted: true});
     });
+
   };
 
   componentDidMount() {
@@ -26,8 +30,13 @@ class NewPost extends Component {
   }
 
   render() {
+    let redirect = null;
+    if(this.state.submitted){
+      redirect = <Redirect to="/posts"/>
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
